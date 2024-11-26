4935070fc032a8cfa418c60b2d8defc0303a61ed
dwniest1903 <gabrieladewani19@gmail.com>
2024-11-27 06:08:18 +0700

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2120584.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 205290008, 205290009, 205290032, 205290012, 205290013, 205290014, 205290015, 205290016, 205290018, 205290019, 205290020, 205290021, 205290023, 205290024 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/205290029/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/205290030/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/205290028/1/ammo.js', 'preload' : true},
+];
